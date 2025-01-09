d1a92bf161b2358692bc145e45c849c2ab41a480
Jim <real_email@example.com>
2025-01-09 14:57:53 -0500

    force failure in main.c

--- main.c
+++ main.c
@@ -28,8 +28,8 @@ int main(void)
     add_test();
     mul_test();
 
-//    printf("hello from main.c, returning 3...\n");
-//    return 3; // Will trigger failure.
-    printf("hello from main.c\n");
+    printf("hello from main.c, returning 3...\n");
+    return 3; // Will trigger failure.
+//    printf("hello from main.c\n");
     return 0;
 }
