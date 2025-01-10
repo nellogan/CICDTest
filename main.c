c750dd93f6f308b3981a2c8daefed3439bbb3389
Jim <real_email@example.com>
2025-01-10 17:08:52 -0500

    127

--- main.c
+++ main.c
@@ -30,6 +30,6 @@ int main(void)
 
 //    printf("hello from main.c, returning 3...0!\n");
 //    return 3; // Will trigger failure.
-    printf("hello from main.c, 126\n");
+    printf("hello from main.c, 127\n");
     return 0;
 }
